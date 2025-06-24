89be5e6419a4d904610c63e67dad7f3ca0e968a2
gabbymorgan <gabriellarosemorgan@gmail.com>
2025-06-24 14:03:30 -0500

    merge issue

--- nomadnet/ui/epaperui/Conversations.py
+++ nomadnet/ui/epaperui/Conversations.py
@@ -13,17 +13,6 @@ class ConversationsDisplay(Component):
     def __init__(self, app, parent):
         super().__init__(app, parent)
         self.title = "Conversations"
-<<<<<<< Updated upstream
-        self.conversations_list = ConversationsListDisplay(self.app, self)
-        self.selected_conversation = SelectedConversationDisplay(self.app, self)
-        self.sub_displays = [self.conversations_list, self.selected_conversation]
-        self.current_display = None
-
-    def start(self):
-        return
-
-    def render(self):
-=======
         self.list_display = ConversationsListDisplay(self.app, self)
         self.conversation_display = SelectedConversationDisplay(self.app, self)
         self.sub_displays = [self.list_display, self.conversation_display]
@@ -32,26 +21,12 @@ class ConversationsDisplay(Component):
     def start(self):
         current_display = self.sub_displays[self.current_display_index]
         current_display.start()
->>>>>>> Stashed changes
         return
 
 class ConversationsListDisplay(Component):
     def __init__(self, app, parent):
         super().__init__(app, parent)
 
-<<<<<<< Updated upstream
-    def render():
-        return
-
 class SelectedConversationDisplay(Component):
     def __init__(self, app, parent):
         super().__init__(app, parent)
-
-    def render():
-        #render the display using all data attributes on self
-        return
-=======
-class SelectedConversationDisplay(Component):
-    def __init__(self, app, parent):
-        super().__init__(app, parent)
->>>>>>> Stashed changes
